8e2dc5c34fd161710bb0ee6581babc7a9a5d011e
t24112541 <t24112541@gmail.com>
2019-02-03 23:20:37 +0700

    object_control 90%

--- api/object_control.js
+++ api/object_control.js
@@ -4,10 +4,10 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/img/')
+    cb(null, './public/img/object_control')
   },
   filename: function (req, file, cb) {
-    cb(null, "veh" + '-' + Date.now()+".jpg")
+    cb(null, "veh-oc" + '-' + Date.now()+".jpg")
   }
 })
 
@@ -46,7 +46,7 @@ router.post('/add_item_object_control',async(req,res)=>{
 
 ////////////////////////////// object_control /////////////////////////////////
 //////////////////////////// add_object_control ///////////////////////////////
-router.post('/add_object_control',async (req, res)=>{
+router.post('/add_object_control',upload.any(),async (req, res)=>{
   try{
     console.log(req.body.oc_u_id)
     let db=req.db
@@ -58,18 +58,40 @@ router.post('/add_object_control',async (req, res)=>{
       oc_status:"ผิดระเบียบ",
       itm_oc_id:req.body.itm_oc_id,
     })
+    // for(let i=0;i<req.files.length;i++){
+    if(req.files.length>0){
+      let img=await req.db("pk_img").insert({
+        	img_img:req.files[0].filename || "",
+          u_code:itm,
+          u_table:"pk_object_control",
+      })
+    }
+    // else{
+    //   let img=await req.db("pk_img").insert({
+    //     	img_img:"",
+    //       u_code:itm,
+    //       u_table:"pk_object_control",
+    //     })
+    // }
+    // // }
+
     res.send({ok:true,txt:"ทำการแจ้งสิ่งผิดระเบียบแล้ว",alt:"success"})
   }catch(e){res.send({ok:false,txt:e.message,alt:"error"})}
 })
 
 //////////////////////////// update_object_control ///////////////////////////////
-router.post('update_object_control',async(req,res)=>{
+router.post('/update_object_control',async(req,res)=>{
   try{
     let db=req.db
     let itm=await db('pk_object_control').update({
       oc_status:req.body.oc_status,
     }).where('oc_id',req.body.oc_id)
-  }catch(e){res.send({ok:false,error:e.message})}
+    res.send({
+      ok: true,
+      txt:"ผ่านการตรวจแก้แล้ว",
+      alt:"success"
+    })
+  }catch(e){res.send({ok:false,error:e.message,txt:"ไม่สามารถแก้ไขข้อมูลได้",alt:"error"})}
 })
 
 router.post('/list', async (req, res) => {
@@ -81,7 +103,9 @@ router.post('/list', async (req, res) => {
     .innerJoin("pk_item_object_control","pk_item_object_control.itm_oc_id","pk_object_control.itm_oc_id")
 
     let stp1=await db('pk_object_control').count('oc_id as count').where("oc_status","ผิดระเบียบ").where("oc_u_table","=",req.body.cv_filter)
-    let stp2=await db('pk_object_control').count('oc_id as count').where("oc_status","แก้ไขแล้ว").where("oc_u_table","=",req.body.cv_filter)
+    let stp2=await db('pk_object_control').count('oc_id as count').where("oc_status","รอการตรวจสอบ").where("oc_u_table","=",req.body.cv_filter)
+    let stp3=await db('pk_object_control').count('oc_id as count').where("oc_status","ผ่านการตรวจสอบ").where("oc_u_table","=",req.body.cv_filter)
+
     //
     let machines=await db("pk_object_control").count("oc_id as count").where("oc_u_table","pk_machine").where("oc_status","ผิดระเบียบ")
     let accessories=await db("pk_object_control").count("oc_id as count").where("oc_u_table","pk_accessories").where("oc_status","ผิดระเบียบ")
@@ -90,7 +114,7 @@ router.post('/list', async (req, res) => {
       res.send({
         ok: true,
         datas:rows,
-        stp1,stp2,machines,accessories,
+        stp1,stp2,stp3,machines,accessories,
       })
 
   } catch (e) {
@@ -102,8 +126,10 @@ router.post('/list', async (req, res) => {
 /////////////////////////  sh_object_control  /////////////////////////////////
 router.post('/sh_object_control', async (req, res) => {
   try {
+    let db=req.db
     let machine = await req.db('pk_machine').select(
       "pk_object_control.oc_id",
+      "pk_object_control.oc_u_id",
       "pk_object_control.oc_u_table",
       "pk_object_control.oc_oc_u_id",
       "pk_object_control.oc_oc_u_table",
@@ -111,7 +137,7 @@ router.post('/sh_object_control', async (req, res) => {
       "pk_object_control.oc_update_date",
       "pk_object_control.oc_status",
       "pk_item_object_control.itm_oc_name",
-      "pk_machine.mc_id",
+      "pk_machine.mc_id as u_id",
       "pk_machine.mc_code as oc_name",
       "pk_machine.mc_brand",
       "pk_machine.mc_series",
@@ -136,7 +162,7 @@ router.post('/sh_object_control', async (req, res) => {
       "pk_object_control.oc_update_date",
       "pk_object_control.oc_status",
       "pk_item_object_control.itm_oc_name",
-      "pk_accessories.ac_id",
+      "pk_accessories.ac_id as u_id",
       "pk_accessories.ac_name as oc_name",
       "pk_accessories.ac_description",
       "pk_accessories.ac_u_id",
@@ -150,20 +176,26 @@ router.post('/sh_object_control', async (req, res) => {
     .where("pk_object_control.oc_id","=",req.body.oc_id)
     // // .where("pk_img.u_table","pk_missing")
 
-    console.log(machine)
-    console.log(accessories)
+
+    // console.log(machine[0].mc_id)
+    // console.log(accessories)
     if(machine.length==1){
+      let img=await db("pk_img").select("*").where("u_code","=",machine[0].oc_id).where("u_table","pk_object_control")
+      console.log(img)
       res.send({
         ok: true,
         datas: machine,
-        type:"พาหนะ ทะเบียน "
+        type:"พาหนะ ทะเบียน ",
+        img
       })
     }
     if(accessories.length==1){
+      let img=await db("pk_img").select("*").where("u_code","=",accessories[0].oc_id).where("u_table","pk_object_control")
       res.send({
         ok: true,
         datas: accessories,
-        type:"อุปกรณ์"
+        type:"อุปกรณ์",
+        img
       })
     }
 
